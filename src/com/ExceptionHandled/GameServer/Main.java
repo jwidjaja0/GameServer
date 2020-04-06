374b6a49874db6c01bee8ea78e964bfb579bbbfa
Joseph Widjaja <jwidjaja0@saddleback.edu>
2020-04-06 00:33:31 -0700

    moved sql to server

--- src/com/ExceptionHandled/GameServer/Main.java
+++ src/com/ExceptionHandled/GameServer/Main.java
@@ -5,46 +5,48 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main2(String[] args){
+    public static void main(String[] args){
         Server gameServer = new Server();
 
         Scanner in = new Scanner(System.in);
+
+
+
         int a = 1;
         while(a != 0){
             a = in.nextInt();
         }
     }
 
+        public static void main2 (String[]args) throws SQLException {
 
-    public static void main(String[] args) throws SQLException {
+            Scanner input = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
-
-        //String URL ="jdbc:mysql://localhost:3306/world";
-        String URL2 = "jdbc:mysql://127.0.0.1:3306/";
-        String user = "guest";
-        String pw = "mypassword";
+            //String URL ="jdbc:mysql://localhost:3306/world";
+            String URL2 = "jdbc:mysql://127.0.0.1:3306/";
+            String user = "guest";
+            String pw = "mypassword";
 //        String pw = input.nextLine();
 
 //        String query = "use 4blogin; ";
 ////                "INSERT INTO playerinfo ";
 ////                "VALUES (default, 'John', 'passw')";
 
-        String query = "select * from 4blogin.playerinfo;";
+            String query = "select * from 4blogin.playerinfo;";
 
-        String query2 = "INSERT INTO 4blogin.playerinfo values(default, 'John','passw')";
+            String query2 = "INSERT INTO 4blogin.playerinfo values(default, 'John','passw')";
 
-        //1. Get a connection to the database
-        Connection myConn = DriverManager.getConnection(URL2, user, pw);
-        //2. Create a statement
-        Statement myStatement = myConn.createStatement();
-        //3. Execute SQL Query
-       // myStatement.execute(query);
-        //ResultSet myRs = myStatement.executeQuery(query);
-        myStatement.executeUpdate(query2);
-        //4. Process the result set
+            //1. Get a connection to the database
+            Connection myConn = DriverManager.getConnection(URL2, user, pw);
+            //2. Create a statement
+            Statement myStatement = myConn.createStatement();
+            //3. Execute SQL Query
+            // myStatement.execute(query);
+            //ResultSet myRs = myStatement.executeQuery(query);
+            myStatement.executeUpdate(query2);
+            //4. Process the result set
 //        while(myRs.next()){
 //            System.out.println(myRs.getString(1) + ", " + myRs.getString(2));
-//        }
-    }}
+        }
+    }
 
--- src/com/ExceptionHandled/GameServer/Server.java
+++ src/com/ExceptionHandled/GameServer/Server.java
@@ -7,6 +7,10 @@ import com.ExceptionHandled.GameMessages.Wrappers.Packet;
 
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -22,6 +26,8 @@ public class Server implements Runnable {
 
     private ListenNewClient listenNewClient;
 
+    private Connection connection;
+
     private Thread thread;
 
     public Server() {
@@ -34,6 +40,8 @@ public class Server implements Runnable {
         GameRoom gm = new GameRoom();
         gameRoomList.add(gm);
 
+
+
         thread = new Thread(this);
         thread.start();
         System.out.println("server instantiated");
@@ -42,8 +50,10 @@ public class Server implements Runnable {
     @Override
     public void run() {
         System.out.println("Server thread started");
+
         while (true){
             try {
+                connection = setConnection();
                 ServerPacket serverPacket = messageQueue.take();
                 Packet packet = serverPacket.getPacket();
 
@@ -56,7 +66,7 @@ public class Server implements Runnable {
 
 
 
-            } catch (InterruptedException | IOException e) {
+            } catch (InterruptedException | IOException | SQLException e) {
                 e.printStackTrace();
             }
         }
@@ -67,13 +77,33 @@ public class Server implements Runnable {
         System.out.println("Connection request from client");
     }
 
-    public void handleSignupRequest(ServerPacket serverPacket) throws IOException {
-        SignUpSuccess signUpSuccess = new SignUpSuccess();
-        Packet packet = new Packet("SignUpSuccess", signUpSuccess);
+    public void handleSignupRequest(ServerPacket serverPacket) throws IOException, SQLException {
+        Packet packet = serverPacket.getPacket();
+        SignUpRequest request = (SignUpRequest) packet.getMessage();
+        String usernameRequest = request.getUsername();
+        String passwordRequest = request.getPassword();
 
-        for(ClientConnection c: clientConnectionList){
-            c.getObjectOutputStream().writeObject(packet);
-        }
+        String query2 = "INSERT INTO 4blogin.playerinfo values(default, 'Jan','paree')";
+        Statement myStatement = connection.createStatement();
+        myStatement.executeUpdate(query2);
 
+
+//        SignUpSuccess signUpSuccess = new SignUpSuccess();
+//        Packet packet = new Packet("SignUpSuccess", signUpSuccess);
+
+//        for(ClientConnection c: clientConnectionList){
+//            c.getObjectOutputStream().writeObject(packet);
+//        }
+
+    }
+
+    public Connection setConnection() throws SQLException {
+        String URL = "jdbc:mysql://127.0.0.1:3306/";
+        String user = "guest";
+        String pw = "mypassword";
+
+        String query = "select * from 4blogin.playerinfo;";
+        Connection myConn = DriverManager.getConnection(URL, user, pw);
+        return myConn;
     }
 }
