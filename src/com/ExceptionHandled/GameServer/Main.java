978d2c2593d7b9e3fc387baf78971246abd6f267
Joseph Widjaja <jwidjaja0@saddleback.edu>
2020-04-05 22:04:35 -0700

    can add to mysql databse

--- src/com/ExceptionHandled/GameServer/Main.java
+++ src/com/ExceptionHandled/GameServer/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main2(String[] args){
         Server gameServer = new Server();
 
         Scanner in = new Scanner(System.in);
@@ -16,29 +16,35 @@ public class Main {
     }
 
 
-    public static void main2(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException {
 
         Scanner input = new Scanner(System.in);
 
         //String URL ="jdbc:mysql://localhost:3306/world";
-        String URL2 = "jdbc:mysql://127.0.0.1:3306/?user=root";
-        String user = "root";
-        String pw = input.nextLine();
+        String URL2 = "jdbc:mysql://127.0.0.1:3306/";
+        String user = "guest";
+        String pw = "mypassword";
+//        String pw = input.nextLine();
 
-        String query = "use 4blogin; " +
-                "INSERT INTO playerinfo " +
-                "VALUES (default, 'John', 'passw')";
+//        String query = "use 4blogin; ";
+////                "INSERT INTO playerinfo ";
+////                "VALUES (default, 'John', 'passw')";
+
+        String query = "select * from 4blogin.playerinfo;";
+
+        String query2 = "INSERT INTO 4blogin.playerinfo values(default, 'John','passw')";
 
         //1. Get a connection to the database
         Connection myConn = DriverManager.getConnection(URL2, user, pw);
         //2. Create a statement
         Statement myStatement = myConn.createStatement();
         //3. Execute SQL Query
-        myStatement.executeUpdate(query);
-//        ResultSet myRs = myStatement.executeQuery(query);
+       // myStatement.execute(query);
+        //ResultSet myRs = myStatement.executeQuery(query);
+        myStatement.executeUpdate(query2);
         //4. Process the result set
 //        while(myRs.next()){
 //            System.out.println(myRs.getString(1) + ", " + myRs.getString(2));
-        }
-    }
+//        }
+    }}
 
